4a3361e3793198d906730c45cfb1736e9f54aad2
Ken Hancock <hancockks@gmail.com>
2020-03-19 22:18:42 -0400

    move fonts to direct download from github; add additional fonts

---
+++ download-fonts.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+wget --no-clobber  --directory-prefix=tmp "https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/Inconsolata/complete/Inconsolata%20Bold%20Nerd%20Font%20Complete%20Windows%20Compatible.ttf"
+wget --no-clobber  --directory-prefix=tmp "https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/Inconsolata/complete/Inconsolata%20Regular%20Nerd%20Font%20Complete%20Windows%20Compatible.ttf"
+
+wget --no-clobber  --directory-prefix=tmp "https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/FiraCode/Regular/complete/Fira%20Code%20Regular%20Nerd%20Font%20Complete%20Windows%20Compatible.ttf"
+wget --no-clobber  --directory-prefix=tmp "https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/FiraCode/Bold/complete/Fira%20Code%20Bold%20Nerd%20Font%20Complete%20Windows%20Compatible.ttf"
+
+wget --no-clobber  --directory-prefix=tmp "https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/FiraCode/Regular/complete/Fira%20Code%20Regular%20Nerd%20Font%20Complete%20Windows%20Compatible.ttf"
+wget --no-clobber  --directory-prefix=tmp "https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/FiraCode/Regular/complete/Fura%20Code%20Regular%20Nerd%20Font%20Complete%20Windows%20Compatible.ttf"
+wget --no-clobber  --directory-prefix=tmp "https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/FiraCode/Bold/complete/Fira%20Code%20Bold%20Nerd%20Font%20Complete%20Windows%20Compatible.ttf"
+wget --no-clobber  --directory-prefix=tmp "https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/FiraCode/Bold/complete/Fura%20Code%20Bold%20Nerd%20Font%20Complete%20Windows%20Compatible.ttf"
+
+wget --no-clobber  --directory-prefix=tmp "https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/Meslo/S/Regular/complete/Meslo%20LG%20S%20Regular%20Nerd%20Font%20Complete%20Windows%20Compatible.ttf"
+wget --no-clobber  --directory-prefix=tmp "https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/Meslo/S/Bold/complete/Meslo%20LG%20S%20Bold%20Nerd%20Font%20Complete%20Windows%20Compatible.ttf"
+wget --no-clobber  --directory-prefix=tmp "https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/Meslo/S/Italic/complete/Meslo%20LG%20S%20Italic%20Nerd%20Font%20Complete%20Windows%20Compatible.ttf"
+wget --no-clobber  --directory-prefix=tmp "https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/Meslo/S/Bold-Italic/complete/Meslo%20LG%20S%20Bold%20Italic%20Nerd%20Font%20Complete%20Windows%20Compatible.ttf"
